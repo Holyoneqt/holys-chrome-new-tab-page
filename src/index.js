57fd751e2e1cdb607386deac291c3245bc6062d5
Holyoneqt <spuehler.luca@gmail.com>
2024-09-18 14:21:00 +0200

    added more links + small style changes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import { getBackgroundImage } from './components/background.js';
+import { initLinks } from './components/links.js';
 import { startTimeInterval } from './components/time.js';
 import { getWeather } from './components/weather.js';
 
 window.onload = () => {
 	// Set Background-Image
 	document.getElementById('background-image').src = getBackgroundImage();
+
+	// Init Links functionality
+	initLinks();
 
 	// Start Interval to update Time every 3 Seconds
 	startTimeInterval();
