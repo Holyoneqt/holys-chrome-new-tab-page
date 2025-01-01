0c8cd0e40a901a21f86c19562648d41f48fc4f8e
Luca Spühler <spuehler.luca@gmail.com>
2025-01-01 20:17:49 +0100

    added Garmin Link & changed font

--- src/components/links.js
+++ src/components/links.js
@@ -1,13 +1,13 @@
 export function initLinks() {
-    const linksToggleButton = document.querySelector("#links-toggle");
-    const linksGrid = document.querySelector(".js-links-grid");
-    linksToggleButton.onclick = () => {
-        if (linksGrid.hasAttribute("collapsed")) {
-            linksGrid.removeAttribute("collapsed");
-            linksGrid.setAttribute("expanded", true);
-        } else {
-            linksGrid.removeAttribute("expanded");
-            linksGrid.setAttribute("collapsed", true);      
-        }
-    };
+  const linksToggleButton = document.querySelector("#links-toggle");
+  const linksGrid = document.querySelector(".js-links-grid");
+  linksToggleButton.onclick = () => {
+    if (linksGrid.hasAttribute("collapsed")) {
+      linksGrid.removeAttribute("collapsed");
+      linksGrid.setAttribute("expanded", true);
+    } else {
+      linksGrid.removeAttribute("expanded");
+      linksGrid.setAttribute("collapsed", true);
+    }
+  };
 }
